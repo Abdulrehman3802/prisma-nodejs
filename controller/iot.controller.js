362a8f96cf30f658f358155f390b5b35aefd7265
Abdulrehman <203802bsit@gmail.com>
2024-03-25 16:42:29 +0500

    prisma implementation with code first approach

--- controller/iot.controller.js
+++ controller/iot.controller.js
@@ -2,46 +2,25 @@ const {PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient()
 
 // module.exports.getData = async (req, res) => {
-//     const data = await prisma.customers.updateMany({
-//         where:{
-//             customerid:{in:req.body.ids},
-//         },
-//         data:{
-//             credit:0
-//         }
-//     }
-//     )
-//     res.json(data)
-// }
-// module.exports.getData = async (req, res) => {
-//     const data = await prisma.customers.findFirst({
-//         where:{
-//                 customerid:{in:req.body.ids},
-//             }
-//         }
-//     )
-//     res.json(data)
-// }
-// module.exports.getData = async (req, res) => {
-//     const data = await prisma.customers.findMany({
-//
+//     const data = await prisma.user.findMany({
 //         }
 //     )
 //     res.json(data)
 // }
 
+
 module.exports.getData = async (req, res) => {
-    const data = await prisma.customers.updateMany({
-            where:{
-                customerid:{in:req.body.ids},
-            },
+    const data = await prisma.user.create({
         data:{
-                is_deleted:false
+            id:1,
+            name:"Ali",
+            email:"ali@gmail.com"
         }
         }
     )
     res.json(data)
 }
+
 module.exports.hello = async (req,res)=>{
     res.json("Hello");
 }
